fec5ba4e34fa896797d7bb39a4a9df227eb5dfc8
hinata356 <piano.natsu@gmail.com>
2023-05-13 12:16:19 +1000

    Revert "payment ui"
    This reverts commit 7589be04409f8cfe942d46440f0d94753b381123.

--- ui/payment_select_view.py
+++ ui/payment_select_view.py
@@ -1,46 +1,33 @@
 import tkinter as tk
-import webbrowser
 
-from payment.payment_handler import PaymentHandler
-from payment.payment_strategy import CardPaymentStrategy
+from order.order import Order
+from order.order_item import OrderItem
+from payment.strategy.card_strategy import CardPaymentStrategy
 
 
 class PaymentSelectView(tk.Toplevel):
 
     def __init__(self, parent, cost: int, title='Payment Method', size='500x500+300+300'):
         super().__init__(parent)
-        self._payment_handler = None
+        self._payment_strategy = None
         self.title(title)
         self.geometry(size)
 
         self.cost = cost
 
-        self.pay_by_cash_btn = tk.Button(master=self, text='Pay By Cash', command=self.on_select_pay_by_cash)
-        self.pay_by_cash_btn.pack()
-
         self.pay_by_card_btn = tk.Button(master=self, text='Pay By Card', command=self.on_select_pay_by_card)
         self.pay_by_card_btn.pack()
-
-        self.result = tk.Label()
-        self.result.pack()
 
     def on_select_pay_by_cash(self):
         pass
 
     def on_select_pay_by_card(self):
-        self._payment_handler = PaymentHandler(CardPaymentStrategy())
-        self.open_stripe_payment_page()
-
-    def get_payment_link(self) -> str:
-        # Call to server to get a payment link
-        link = 'https://buy.stripe.com/test_bIY8y37dN0Jj57a8ww'
-        return link
-
-    def show_response_message(self, message):
-        self.result.config(text=message)
-
-    def open_stripe_payment_page(self):
-        link = self.get_payment_link()
-        webbrowser.open(link)
-        self._payment_handler.register_success_callback(callback=lambda _: self.show_response_message('Success'))
-        self._payment_handler.register_failure_callback(callback=lambda _: self.show_response_message('Failed'))
+        self._payment_strategy = CardPaymentStrategy()
+        mocked_order = Order(
+            items=[
+                OrderItem(item_name='Pizza', item_quantity=1, item_price=2000),
+                OrderItem(item_name='Spagetti', item_quantity=1, item_price=1000),
+            ]
+        )
+        self._payment_strategy.process_payment(mocked_order)
+        self.destroy()
--- virtual_server.py
+++
@@ -1,7 +0,0 @@
-class VirtualServer:
-    def __init__(self):
-        pass
-
-
-def start_virtual_server_in_different_process():
-    pass
